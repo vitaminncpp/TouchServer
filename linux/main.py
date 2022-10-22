3f73d423af0b38fa7f1ea29b0793df8d173832b0
SKR301 <skrint301@gmail.com>
2022-10-22 23:45:57 +0530

    #7 POC is working, receiving data, broadcasting IP

--- linux/main.py
+++ linux/main.py
@@ -8,24 +8,25 @@ ECHO_PORT = 5560
 SERVER_PORT = 5559
 
 def echo():
-    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
-    sock2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock2.bind((ip_broadcast, ECHO_PORT))
-    sock2.sendto(b'x', ("255.255.255.255", ECHO_PORT))
-    sock2.close()
-    sleep(3)
+    while(True):
+        print(1)
+        sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+        sock2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock2.bind((ip_broadcast, ECHO_PORT))
+        sock2.sendto(b'x', ("255.255.255.255", ECHO_PORT))
+        sock2.close()
+        sleep(3)
     
 def server():
-    sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
-    sock1.bind((ip_server, SERVER_PORT))
-    data, addr = sock1.recvfrom(1024)
-    print("Message: ", data)
+    while(True):
+        print(2)
+        sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+        sock1.bind((ip_server, SERVER_PORT))
+        data, addr = sock1.recvfrom(1024)
+        print("Message: ", data)
 
-while(True):
-    x1 = threading.Thread(target=echo)
-    x2 = threading.Thread(target=server)
+x1 = threading.Thread(target=echo)
+x2 = threading.Thread(target=server)
 
-    # echo()
-    # server()
-    x1.start()
-    x2.start()
+x1.start()
+x2.start()
--- linux/receiver.py
+++ linux/receiver.py
@@ -2,8 +2,13 @@ import socket
 from time import sleep
 import threading
 
-ip = '192.168.1.10'
-ECHO_PORT = 5560
+def getNetworkIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.connect(('<broadcast>', 0))
+    return s.getsockname()[0]
+
+ip = getNetworkIp()
 SERVER_PORT = 5559
 
 while(True):
