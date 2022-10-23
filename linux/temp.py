84fd6d2c1a215af024e04e64edf06cd790e64dfa
SKR301 <skrint301@gmail.com>
2022-10-23 20:54:55 +0530

    #7 updated gitignore

--- linux/temp.py
+++
@@ -1,8 +0,0 @@
-import socket
-def getNetworkIp():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.connect(('<broadcast>', 0))
-    return s.getsockname()[0]
-
-print (getNetworkIp())
