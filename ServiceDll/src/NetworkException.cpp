7ad3a9ca88a7b9b7736ed43ab98e5ac57d1cac65
Akshayaap <akshaykumarparmar51@gmail.com>
2023-01-02 20:03:00 +0530

    Rebranding

---
+++ ServiceDll/src/NetworkException.cpp
@@ -0,0 +1,28 @@
+
+#include "NetworkException.h"
+
+NetworkException::NetworkException(std::string message, std::string file, int line)
+{
+    this->message = message.append("::File:").append(file).append("::Line:").append(std::to_string(line));
+    this->file = file;
+    this->line = line;
+}
+
+NetworkException::~NetworkException()
+{
+}
+
+const char *NetworkException::what() noexcept
+{
+    return this->message.c_str();
+}
+
+std::string NetworkException::getFileName()
+{
+    return this->file;
+}
+
+int NetworkException::getLine()
+{
+    return this->line;
+}
---
+++ ServiceDll/src/UDPReceiver.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Network.h"
+
+class UDPReceiver
+{
+private:
+    SOCKET sock;
+    WSAData wsaData;
+    sockaddr_in toRecv;
+    unsigned short port;
+    int addrSize;
+
+public:
+    UDPReceiver();
+    UDPReceiver(unsigned short int port);
+    UDPReceiver(IPv4 ip);
+    UDPReceiver(IPv4 ip, unsigned short int port);
+    ~UDPReceiver();
+
+public:
+    int Bind(IPv4 ip, unsigned short int port);
+    int Bind(IPv4 ip);
+    int Bind(unsigned short int port);
+
+public:
+    int Recv(char *data, int size, int flags = 0);
+    int Recv(char *data, int size, IPv4 ip, int flags = 0);
+    int Recv(char *data, int size, IPv4 ip, unsigned short int port, int flags = 0);
+};
